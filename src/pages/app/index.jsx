dcc25c6eba679856e4f3d714311d6fdee41c4500
brunomachadors <brunomachadors@gmail.com>
2023-10-06 23:49:27 +0100

    Adding Routes

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppUi, EmailContainer, LadingPage, Paragraph, Title } from './style';
 import { MainHeader } from '../../components/MainHeader';
 import { DefaultButton } from '../../components/Buttons/style';
 import { EmailInput } from '../../components/Input';
+import Login from '../login';
 
 const App = () => {
   return (
@@ -15,7 +16,13 @@ const App = () => {
             src="https://fontmeme.com/permalink/231005/1bf0ab08da7c21a3ed063a6277666c66.png"
             alt="Bookflix Logo"
           />
-          <DefaultButton style={{ margin: '10px' }}>Sign In</DefaultButton>
+          <DefaultButton
+            style={{ margin: '10px' }}
+            to="/login"
+            relative="/login"
+          >
+            Sign In
+          </DefaultButton>
         </MainHeader>
         <LadingPage>
           <Title>Unlimited books, articles, and more</Title>
@@ -29,12 +36,15 @@ const App = () => {
               type="email"
               placeholder="Type your e-mail"
             ></EmailInput>
-            <DefaultButton heightSize={'55px'} fontSize={'35px'}>
+            <DefaultButton heightSize={'55px'} fontSize={'35px'} to="/register">
               Subscribe
             </DefaultButton>
           </EmailContainer>
         </LadingPage>
       </AppUi>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 };
