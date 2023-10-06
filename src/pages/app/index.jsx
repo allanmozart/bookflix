616b0516ab4311551e42186a9c3d79a6689a11b9
ca-trindade <piratum.core@gmail.com>
2023-10-06 17:36:52 +0100

    Update the css, adding some responsive details

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,51 +1,55 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Navbar, Nav, Button } from 'react-bootstrap';
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 import Login from '../login';
 import './styles.css'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar collapseOnSelect expand="lg">
-        <Navbar.Brand />
-        <Link to="/">
-          <img
-            src="https://fontmeme.com/permalink/231005/1bf0ab08da7c21a3ed063a6277666c66.png"
-            alt="Bookflix Logo"
-          />
-        </Link>
+      <Navbar className="navbar" collapseOnSelect expand="lg">
+        <Navbar.Brand>
+          <Link to="/">
+            <img
+              className="logo"
+              src="https://fontmeme.com/permalink/231005/1bf0ab08da7c21a3ed063a6277666c66.png"
+              alt="Bookflix Logo"
+            />
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Button variant="danger">
-              <Link className="button" to="/login">
-                Sign In
-              </Link>
-            </Button>
+          <Nav className="buttonSingIn">
+            <Link className="button" to="/login">
+              Sign In
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
 
-      <Container>
+      <Container className="mainContainer">
         <Row className="mainContentRow">
           <h1>Unlimited books, articles, and more</h1>
           <p>Read anywhere. Cancel anytime.</p>
-          <p>
+          <p className="pSubscribe">
             Ready to read? Enter your email to create or restart your
             membership.
           </p>
         </Row>
+      </Container>
+      <Container className="mainContentRowform">
         <Row>
-          <Col>
-            <input className="inputEmail" type="email" placeholder="Email address" />
+          <Col className="formCol" xs={1} md={4} lg={7}>
+            <input
+              className="inputEmail"
+              type="email"
+              placeholder="Email address"
+            />
           </Col>
-          <Col>
-            <Button variant="danger">
-              <Link className="button" to="/">
-                Get Started
-              </Link>
-            </Button>
+          <Col className="formCol">
+            <Link className="button" to="/">
+              Get Started >
+            </Link>
           </Col>
         </Row>
       </Container>
