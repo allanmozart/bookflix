665d11cfa1c0ae3b984c84cadd3115b5c9930376
tatiana.taketsuma@gmail.com <tatiana.taketsuma@gmail.com>
2023-10-08 18:59:27 +0100

    adding register page landing page

---
+++ src/pages/RegisterPage/style.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Page = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const Paragraph = styled.p`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  padding-top: 10px;
+  flex-wrap: wrap;
+  font-size: 15px;
+  font-weight: 400;
+  max-width: 200px;
+  font-family: 'Helvetica', sans-serif;
+  color: white;
+`;
