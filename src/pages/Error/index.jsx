58df83157bd199173bf32ad80827f59c2ae8d4bf
ca-trindade <piratum.core@gmail.com>
2023-10-11 19:59:59 +0100

    add a error page

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,30 @@
+import { MainHeader } from "../../components/MainHeader";
+import { Link } from "react-router-dom";
+import { Logo } from "../../components/Logo";
+import { Paragraph, Title } from "./style";
+import { ErrorPageBg } from "./style";
+import { DefaultButton } from "../../components/Buttons/style";
+
+export function ErrorPage() {
+  return (
+    <>
+      <ErrorPageBg>
+        <MainHeader>
+          <Link to="/">
+            <Logo
+              src="https://fontmeme.com/permalink/231005/1bf0ab08da7c21a3ed063a6277666c66.png"
+              alt="Bookflix Logo"
+            ></Logo>
+          </Link>
+        </MainHeader>
+        <Title>Upss! Something went wrong</Title>
+        <Paragraph>
+          Sorry, we could not find the page you were looking for.
+          <DefaultButton style={{ margin: "10px" }} to="/login">
+            Sign In
+          </DefaultButton>
+        </Paragraph>
+      </ErrorPageBg>
+    </>
+  );
+}
