e8e80cd93d5034a32924ff4f4130954e6caf47ec
ca-trindade <piratum.core@gmail.com>
2023-10-12 17:51:20 +0100

    styled the Catalog page

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { RequireAuth } from '../RequireAuth/RequireAuth';
-import getBooks from '../../api/book';
-import { BookList, BookItem, CatalogContainer, BookCover } from './style';
+import { useEffect, useState } from "react";
+import { RequireAuth } from "../RequireAuth/RequireAuth";
+import getBooks from "../../api/book";
+import { BookList, BookItem, CatalogContainer, BookCover } from "./style";
 
 export default function Catalog() {
   const [books, setBooks] = useState([]);
@@ -14,7 +14,7 @@ export default function Catalog() {
         setBooks(bookData.books);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching books:', error);
+        console.error("Error fetching books:", error);
         setLoading(false);
       }
     }
@@ -33,12 +33,12 @@ export default function Catalog() {
           {Array.isArray(books) && books.length > 0 ? (
             books.map((book) => (
               <BookItem key={book.id}>
-                <h2>{book.title}</h2>
+                <h2 style={{ fontSize: "145%" }}>{book.title}</h2>
                 <p>Year: {book.year}</p>
                 <p>Description: {book.description}</p>
                 <BookCover
                   src={book.book_cover}
-                  alt={`${book.title} cover`}
+                  alt={`${book.title}cover`}
                 ></BookCover>
               </BookItem>
             ))
--- src/pages/catalogPage/index.jsx
+++ src/pages/catalogPage/index.jsx
@@ -31,7 +31,7 @@ export default function CatalogPage() {
       </MainHeader>
 
       <Page>
-        <Catalog></Catalog>
+        <Catalog />
       </Page>
     </>
   );
