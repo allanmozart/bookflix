7be12bc7bee6aa378d820c86773c9939f8275477
Alda Oliveira <aldaoliveira35@gmail.com>
2023-10-15 20:05:33 +0100

    Replaces header on my books / new book pages

--- src/pages/MyBooksPage/index.jsx
+++ src/pages/MyBooksPage/index.jsx
@@ -1,38 +1,32 @@
-import { Link } from 'react-router-dom';
-import { MainHeader } from '../../components/MainHeader';
-import MyBooks from '../../components/MyBooks';
-import { RequireAuth } from '../../components/RequireAuth/RequireAuth';
-import { Page, Paragraph } from './style';
-import { Logo } from '../../components/Logo';
-import { DefaultButton } from '../../components/Buttons/style';
-import { Logout } from '../../components/Logout';
-import { useUser } from '../../components/UserContext/User';
+import { Link } from "react-router-dom";
+import MyBooks from "../../components/MyBooks";
+import { RequireAuth } from "../../components/RequireAuth/RequireAuth";
+import { Page } from "./style";
+import { Logo } from "../../components/Logo";
+import { Logout } from "../../components/Logout";
+import { Navbar, NavbarContainer, LinkNavbar } from "../../components/Navbar";
 
 export default function MyBooksPage() {
-  const { userData } = useUser();
-
   return (
     <Page>
       <RequireAuth />
-      <MainHeader>
+      <Navbar>
         <Link to="/">
           <Logo
             src="https://fontmeme.com/permalink/231005/1bf0ab08da7c21a3ed063a6277666c66.png"
             alt="Bookflix Logo"
           ></Logo>
         </Link>
-        <DefaultButton style={{ margin: '10px' }} to="/catalog">
-          Catalog
-        </DefaultButton>
-        <DefaultButton style={{ margin: '10px' }} to="/newbook">
-          Insert New book
-        </DefaultButton>
-        <DefaultButton style={{ margin: '10px' }} to="/mybooks">
-          My Books
-        </DefaultButton>
-        <Logout></Logout>
-      </MainHeader>
-      <Paragraph>{userData.name}</Paragraph>
+        <NavbarContainer>
+          <LinkNavbar href="/catalog">Catalog</LinkNavbar>
+
+          <LinkNavbar href="/mybooks">My Books</LinkNavbar>
+
+          <LinkNavbar href="/newbook">Insert New book</LinkNavbar>
+
+          <Logout />
+        </NavbarContainer>
+      </Navbar>
       <MyBooks></MyBooks>
     </Page>
   );
--- src/pages/NewBookPage/index.jsx
+++ src/pages/NewBookPage/index.jsx
@@ -1,33 +1,33 @@
-import { Logo } from '../../components/Logo';
-import { MainHeader } from '../../components/MainHeader';
-import { Link } from 'react-router-dom';
-import { Page } from './style';
-import NewBook from '../../components/NewBook';
-import { DefaultButton } from '../../components/Buttons/style';
+import { Logo } from "../../components/Logo";
+import { MainHeader } from "../../components/MainHeader";
+import { Link } from "react-router-dom";
+import { Page } from "./style";
+import NewBook from "../../components/NewBook";
+import { DefaultButton } from "../../components/Buttons/style";
+import { Navbar, NavbarContainer, LinkNavbar } from "../../components/Navbar";
 
 export default function NewBookPage() {
   return (
     <Page>
-      <MainHeader>
+      <Navbar>
         <Link to="/">
           <Logo
             src="https://fontmeme.com/permalink/231005/1bf0ab08da7c21a3ed063a6277666c66.png"
             alt="Bookflix Logo"
           ></Logo>
         </Link>
-        <DefaultButton style={{ margin: '10px' }} to="/catalog">
-          Catalog
-        </DefaultButton>
-        <DefaultButton style={{ margin: '10px' }} to="/newbook">
-          Insert New book
-        </DefaultButton>
-        <DefaultButton style={{ margin: '10px' }} to="/mybooks">
-          My Books
-        </DefaultButton>
-        <DefaultButton style={{ margin: '10px' }} to="/login">
-          Logout
-        </DefaultButton>
-      </MainHeader>
+        <NavbarContainer>
+          <LinkNavbar href="/catalog">Catalog</LinkNavbar>
+
+          <LinkNavbar href="/mybooks">My Books</LinkNavbar>
+
+          <LinkNavbar href="/newbook">Insert New book</LinkNavbar>
+
+          <DefaultButton style={{ margin: "3px 10px" }} to="/login">
+            Logout
+          </DefaultButton>
+        </NavbarContainer>
+      </Navbar>
       <NewBook></NewBook>
     </Page>
   );
